a030b2d29cd62edd50c4e912a9b55bdcda5bffdf
Madalin Mamuleanu <mamuleanu.madalin@gmail.com>
2020-04-09 09:19:03 +0300

    Created XCode Demo Project

---
+++ xcode/SwiftDP_Example/SwiftDP_Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftDP_Example
+//
+//  Created by Madalin Mamuleanu on 09/04/2020.
+//  Copyright © 2020 OpenMined. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
